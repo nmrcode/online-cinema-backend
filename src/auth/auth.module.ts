c85b7296253baac10be787483a7c561298843745
nmcode <nmcode.personal@gmail.com>
2022-05-29 22:15:11 +0300

    add: user model и базовая настройка бд

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModel } from '../user/user.model'
+import { ConfigModule } from '@nestjs/config'
+
+@Module({
+  providers: [AuthService],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: UserModel,
+        schemaOptions: {
+          collection: 'User',
+        },
+      },
+    ]),
+    ConfigModule,
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule {}
